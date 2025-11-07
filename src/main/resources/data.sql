0331ba58adefd3e1f9fa15c90245bbf5492a37f8
Denilson Santos <denilson_n.santos@hotmail.com>
2025-11-06 23:54:12 -0300

    chore: add new scheme value

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,6 @@ VALUES ('AP-0001', '123.456.789-09', DATE '2025-01-01', DATE '2026-01-01', 'ABC-
 
 INSERT INTO policies (numero_apolice, cpf_segurado, data_inicio, data_fim, placa_veiculo, valor_premio)
 VALUES ('AP-0002', '987.654.321-00', DATE '2025-06-15', DATE '2026-06-15', 'XYZ-9999', 2300.50);
+
+INSERT INTO policies (numero_apolice, cpf_segurado, data_inicio, data_fim, placa_veiculo, valor_premio)
+VALUES ('AP-0003', '987.654.321-10', DATE '2025-06-25', DATE '2026-06-25', 'ABC-9999', 2000.00);
